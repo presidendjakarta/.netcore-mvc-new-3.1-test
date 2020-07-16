0c2d04390d5eea4001cf1b835df7d6af549595df
Taufiq <presidendjakarta@gmail.com>
2020-07-17 04:09:17 +0700

    add view_buku tamu :v
    Signed-off-by: Taufiq <presidendjakarta@gmail.com>

---
+++ Controllers/CatatControllers.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using testmvc.Models;
+
+namespace testmvc.Controllers
+{
+    public class CatatController : Controller
+    {
+        private readonly ILogger<CatatController> _logger;
+
+        public CatatController(ILogger<CatatController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
