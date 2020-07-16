4bbf498a2dc4cb67ee27a5b569e1840e10eadd36
Taufiq <presidendjakarta@gmail.com>
2020-07-17 03:33:13 +0700

    belajar bikin view
    Signed-off-by: Taufiq <presidendjakarta@gmail.com>

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,12 @@ namespace testmvc.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
